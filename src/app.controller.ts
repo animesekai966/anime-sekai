9d5b3697a19c592a8edd7a78e7f96bb101f6761e
Sekai <50212311+ig4e@users.noreply.github.com>
2022-07-30 12:01:27 +0200

    -

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Param, Res } from "@nestjs/common";
 
-
 @Controller()
 export class AppController {
   constructor() {}
+  @Get("/")
+  home() {
+    return "hi";
+  }
 }
